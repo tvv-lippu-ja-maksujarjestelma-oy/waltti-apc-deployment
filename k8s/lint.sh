368a173ed987560457b8589fbc4f6ecc6c2bb3bf
haphut <haphut@mistmap.com>
2022-09-07 19:42:40 +0300

    FIXME: Add echos to lint script

--- k8s/lint.sh
+++ k8s/lint.sh
@@ -9,6 +9,7 @@ set -Eeuo pipefail
 
 KUSTOMIZE_OVERLAY_DIR="$1"
 
+echo '*** Run yamllint while accepting errors'
 # Expect file extension 'yaml'.
 # Accept yamllint errors.
 yamllint ./base/*.yaml || true
@@ -16,6 +17,12 @@ yamllint ./base/*.yaml || true
   find "${KUSTOMIZE_OVERLAY_DIR}" -type f -iname '*.yaml' -print0 |
     xargs -0 yamllint
 ) || true
+echo '*** Run kubeval'
 ./run-kubeval.sh "${KUSTOMIZE_OVERLAY_DIR}"
+echo '*** Run polaris'
 ./run-polaris.sh "${KUSTOMIZE_OVERLAY_DIR}"
+echo '*** Run kube-score'
 ./run-kube-score.sh "${KUSTOMIZE_OVERLAY_DIR}"
+# kube-linter is not there yet.
+#echo '*** Run kube-linter'
+#./run-kube-linter.sh "${KUSTOMIZE_OVERLAY_DIR}"
