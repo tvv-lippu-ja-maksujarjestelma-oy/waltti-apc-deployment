93320c5f3e2819c5fce7851a877385ad696693e5
Jyrki Ahpola <jyrki.ahpola@futurice.com>
2023-05-25 13:02:34 +0300

    Added waltti-rtpi mqtt user

--- modules/mqtt-user/variables.tf
+++ modules/mqtt-user/variables.tf
@@ -5,3 +5,21 @@ variable "secretname" {
 variable "vhost" {
   type = string
 }
+
+variable "topic_perms_read" {
+  type = string
+}
+
+variable "topic_perms_write" {
+  type = string
+}
+
+variable "user_perms_read" {
+  type = string
+  default = "^([amq\\.topic].*)$"
+}
+
+variable "user_perms_write" {
+  type = string
+  default = "^([amq\\.topic].*)$"
+}
