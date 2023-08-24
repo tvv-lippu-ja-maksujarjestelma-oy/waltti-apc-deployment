a3ec6dfd201f1ab4ff7740881753a877b9d106a2
Jyrki Ahpola <jyrki.ahpola@futurice.com>
2023-08-24 14:54:07 +0300

    Fix mqtt permissions

--- mqtt/environments/dev/users.tf
+++ mqtt/environments/dev/users.tf
@@ -18,8 +18,8 @@ module "pulsar_mqtt_forwarder" {
   source            = "../../../modules/mqtt-user"
   secretname        = "mqtt-pulsar-mqtt-forwarder"
   vhost             = rabbitmq_vhost.apc.name
-  topic_perms_read  = "^apc\\.v[0-9][0-9]*\\.authority-id\\..*$"
-  topic_perms_write = "^apc\\.v[0-9][0-9]*\\.authority-id\\..*$"
+  topic_perms_read  = "^(?:anonymized-)?apc\\.v[0-9][0-9]*\\.authority-id\\..*$"
+  topic_perms_write = "^(?:anonymized-)?apc\\.v[0-9][0-9]*\\.authority-id\\..*$"
 }
 
 module "mqtt_waltti_rtpi" {
