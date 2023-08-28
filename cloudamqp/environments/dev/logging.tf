6523c83579e3f098da315bc21ef9449a9bf7cae8
Jyrki Ahpola <jahpola@users.noreply.github.com>
2023-08-28 11:49:10 +0300

    CloudAMPQ logging into GCP (#79)

---
+++ cloudamqp/environments/dev/logging.tf
@@ -0,0 +1,27 @@
+resource "google_service_account" "cloudamqp_logging" {
+  account_id   = "cloudamp-logging"
+  description  = "CloudAMQP Logger"
+}
+
+resource "google_project_iam_member" "log_writer" {
+    project = var.project_id
+    role    = "roles/logging.logWriter"
+    member  = "serviceAccount:${google_service_account.cloudamqp_logging.email}"
+}
+
+resource "google_project_iam_member" "metric_writer" {
+    project = var.project_id
+    role    = "roles/monitoring.metricWriter"
+    member  = "serviceAccount:${google_service_account.cloudamqp_logging.email}"
+}
+
+
+resource "google_service_account_key" "cloudamqp_key" {
+  service_account_id = google_service_account.cloudamqp_logging.name
+}
+
+resource "cloudamqp_integration_log" "stackdriver" {
+  instance_id = cloudamqp_instance.instance.id
+  name        = "stackdriver"
+  credentials = google_service_account_key.cloudamqp_key.private_key
+}
--- cloudamqp/environments/dev/provider.tf
+++ cloudamqp/environments/dev/provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
     google = {
       source  = "hashicorp/google"
-      version = "~> 4.64.0"
+      version = "~> 4.79.0"
 
     }
   }
